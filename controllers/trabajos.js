74641c1b590f28df9b4644b3822b16a956831f91
DAW-DUCKASWELL <114958225+DAW-DUCKASWELL@users.noreply.github.com>
2022-11-24 01:57:57 -0400

    Programando trabajos
    Añadí los controladores de encontrar trabajo por id, buscar todos los trabajos y por último insertar nuevo trabajo. A su vez cree las rutas correspondientes para cada protocolo.

--- controllers/trabajos.js
+++ controllers/trabajos.js
@@ -1,2 +1,35 @@
 const {modeloTrabajos} = require('../models');
 const { db } = require('../models/nosql/equipos');
+const mongoose = require('mongoose');
+
+/**
+ * Obtener Trabajo by ID
+ */
+ const getTrabajo = async (req, res) =>{
+    const {_id} = req.params;
+    const data = await modeloTrabajos.findById(_id);
+    res.send({data});
+};
+
+/**
+ * Obtener lista de Trabajos disponibles
+ */
+const getTrabajos = async (req, res) =>{
+    const data = await modeloTrabajos.find({});
+    res.send({data});
+    
+};
+
+/**
+ * Crear un Trabajo nuevo
+ */
+const postTrabajos = (req, res) =>{
+    const _id = mongoose.Types.ObjectId();
+    const trabajos = modeloTrabajos(req.body);
+    trabajos
+    .save()
+    .then((data)=> res.json(data))
+    .catch((error=> res.json({message: error})));
+};
+
+module.exports = {getTrabajo, getTrabajos, postTrabajos}
--- routes/trabajos.js
+++ routes/trabajos.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const {getTrabajos, getTrabajo, postTrabajos} = require('../controllers/trabajos')
+
+//RUTAS CRUD
+
+router.get('/', getTrabajos); 
+
+router.post('/', postTrabajos);
+
+router.get('/:_id', getTrabajo);
 
 module.exports = router
